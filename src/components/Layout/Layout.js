7bf6424992d62a6f285eba40419662d10bdfb5e9
unknown <adek236@gmail.com>
2022-09-05 12:56:48 +0200

    Day&Night font color added.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Layout.css";
 
 const Layout = ({ children }) => {
-  return <div className="container">{children}</div>;
+  return <div className="container font-color-light">{children}</div>;
 };
 
 export default Layout;
