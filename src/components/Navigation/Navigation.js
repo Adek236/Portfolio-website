5faac2e46e06839d2c11613239c62a905a71db91
unknown <adek236@gmail.com>
2022-09-06 16:48:12 +0200

    Navigation bar styled.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,8 @@
 import "./Navigation.css";
 import MenuIcon from "@mui/icons-material/Menu";
-import DiamondIcon from "@mui/icons-material/Diamond";
-import PersonIcon from "@mui/icons-material/Person";
+// import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LanguageIcon from "@mui/icons-material/Language";
 
 function NavBar({ showModal, setShowModal, modalRef, navigationRef }) {
   // const toggleShowModal = () => {
@@ -9,37 +10,38 @@ function NavBar({ showModal, setShowModal, modalRef, navigationRef }) {
   // };
 
   return (
-    <div className="navigation">
-      <nav aria-label="Primary">
-        <div>
+      <nav className="navigation" aria-label="Primary">
+        <div className="navigation__logo">
           <a role="button" aria-label="Login" href="/">
-            <PersonIcon />
+            Adrian Zawadzki
           </a>
         </div>
-        <div>
-          <a role="button" aria-label="Home page" href="/">
-            <DiamondIcon />
-          </a>
-        </div>
-        <div
-          role="button"
-          aria-label="Open modal"
-          tabIndex="0"
-          // ref={navigationRef}
-          // onClick={() => toggleShowModal()}
-          // onKeyDown={(e) => {
-          //   if (e.key === "Enter") {
-          //     toggleShowModal();
-          //   }
-          //   if (e.key === "Tab" && showModal && !e.shiftKey) {
-          //     modalRef.current.focus();
-          //   }
-          // }}
-        >
-          <MenuIcon />
+        <div className="navigation__icons">
+          <div>
+            <DarkModeIcon />
+          </div>
+          <div>
+            <LanguageIcon />
+          </div>
+          <div
+            role="button"
+            aria-label="Open modal"
+            tabIndex="0"
+            // ref={navigationRef}
+            // onClick={() => toggleShowModal()}
+            // onKeyDown={(e) => {
+            //   if (e.key === "Enter") {
+            //     toggleShowModal();
+            //   }
+            //   if (e.key === "Tab" && showModal && !e.shiftKey) {
+            //     modalRef.current.focus();
+            //   }
+            // }}
+          >
+            <MenuIcon />
+          </div>
         </div>
       </nav>
-    </div>
   );
 }
 
