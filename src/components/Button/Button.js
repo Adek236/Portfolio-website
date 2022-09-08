ebd1c7767d0073ba67564735593d21288aa8276f
unknown <adek236@gmail.com>
2022-09-08 23:15:44 +0200

    Swpier for slider added.

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,15 +1,28 @@
 import React from "react";
 import "./Button.css";
+import { motion } from "framer-motion";
 
 const Button = ({ shortTitle, longTitle, children, link }) => {
   return (
-    <div className="button">
-      <div className="button__short-title">{shortTitle?shortTitle:""}{children}</div>
+    <motion.div
+      whileHover={{ scale: 1.1 }}
+      transition={{ type: "spring", stiffness: 400, damping: 10 }}
+      className="button"
+    >
+      <div className="button__short-title">
+        {shortTitle ? shortTitle : ""}
+        {children}
+      </div>
       <div className="button__long-title">{longTitle}</div>
-      <a role="button" aria-label={longTitle} className="fill-container" href={link}>
+      <a
+        role="button"
+        aria-label={longTitle}
+        className="fill-container"
+        href={link}
+      >
         {" "}
       </a>
-    </div>
+    </motion.div>
   );
 };
 
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,22 +1,44 @@
+// import { useState } from "react";
 import "./Projects.css";
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+// import { motion } from "framer-motion";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 const Projects = () => {
   return (
     <div className="project">
       <div className="project__slider">
-       <div className="project__slider__left-arrow flex-center"><KeyboardArrowLeftIcon/></div>
-       <div className="project__slider__elements">
-        <div>SLIDE</div>
-       </div>
-       <div className="project__slider__right-arrow flex-center"><KeyboardArrowRightIcon/></div>
+        <div className="project__slider__left-arrow flex-center">
+          <KeyboardArrowLeftIcon />
+        </div>
+        <div className="project__slider__elements">
+          <div>SLIDE</div>
+        </div>
+        <div className="project__slider__right-arrow flex-center">
+          <KeyboardArrowRightIcon />
+        </div>
       </div>
-      <div className="project__menu">
-        <div  className="project__menu__el">Avatar</div>
-      </div>
+      <Swiper
+        modules={[Navigation]}
+        navigation
+        speed={800}
+        slidesPerView={1}
+        loop
+        className="project__menu"
+      >
+        <SwiperSlide className="project__menu__el">Avatar1</SwiperSlide>
+        <SwiperSlide className="project__menu__el">Avatar2</SwiperSlide>
+        <SwiperSlide className="project__menu__el">Avatar3</SwiperSlide>
+        <SwiperSlide className="project__menu__el">Avatar4</SwiperSlide>
+      </Swiper>
     </div>
-  )
+  );
 };
 
 export default Projects;
